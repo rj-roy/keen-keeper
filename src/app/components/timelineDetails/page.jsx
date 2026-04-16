5a9393194267d007b327210f1dd3f54bf04f44c5
rj-roy <dpjdeveloper.me@gmail.com>
2026-04-16 22:50:43 +0600

    TimeLine Updated

--- src/app/components/timelineDetails/page.jsx
+++ src/app/components/timelineDetails/page.jsx
@@ -1,7 +1,25 @@
-const TimeLineDetails = () => {
+const TimeLineDetails = ({ it }) => {
     return (
-        <div>
-            dlskfj            
+        <div className="">
+            <div className="text-gray-600 bg-white border border-gray-300 flex items-center p-5 rounded-xl">
+                <div className="w-9">
+                    {
+                        it.ctv === "Call" ? (
+                            <span>📞</span>
+                        ) : it.ctv === "Text" ? (
+                            <span>💬</span>
+                        ) : (
+                            <span>🤝</span>
+                        )
+                    }
+                </div>
+                <div className="w-full space-y-1">
+                    <div>
+                        <b className="text-gray-600">{it.description}</b>
+                    </div>
+                    <p>{it.date}</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -2,15 +2,35 @@
 import { Handshake } from "lucide-react";
 import { useInteractions } from "../context/dContext";
 import TimeLineDetails from "../components/timelineDetails/page";
+import { useState } from "react";
 
 const TimeLinePage = () => {
-    const {interactions, setInterActions} = useInteractions();
-    console.log(interactions);
+    const { interactions, setInterActions } = useInteractions();
+    console.log(interactions, "int");
+
+    const [filter, setFilter] = useState("");
+    const filterData = filter ? interactions.filter((i)=> i.ctv === filter) : interactions;
+    console.log(filterData, "fil");
+
     return (
-        <div className="w-full max-w-6xl mx-auto py-15">
-            {
-                interactions.map((t, i) => <TimeLineDetails key={i} t={t}/>)
-            }
+        <div className="bg-[#f8fafc] w-full">
+            <div className="w-full max-w-5xl mx-auto py-10 space-y-4 px-4">
+                <h2 className="text-3xl font-bold">Timeline</h2>
+                <select
+                value={filter}
+                onChange={(e)=> setFilter(e.target.value)}
+                className="bg-white border border-gray-300 p-3 w-50 text-gray-500">
+                    <option value="none">Filter Timeline</option>
+                    <option value="Call">Call</option>
+                    <option value="Text">Text</option>
+                    <option value="Video">Video</option>
+                </select>
+                { filter === "none" ? 
+                    interactions.map((t, i) => <TimeLineDetails key={i} it={t} />) : 
+                    filterData.length === 0 ? <div className="h-[50dvh] grid items-center justify-center text-2xl font-bold">Not Found any Data</div> :
+                    filterData.map((t, i) => <TimeLineDetails key={i} it={t} />)
+                }
+            </div>
         </div>
     );
 };
